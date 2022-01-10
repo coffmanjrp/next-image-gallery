9ad96f15465686aac18abe1e0b624a33adcfa7f2
coffmanjrp <coffmanjrp@gmail.com>
2022-01-09 19:44:23 -0800

    Fix GalleryItem component

--- components/GalleryItem.jsx
+++ components/GalleryItem.jsx
@@ -2,24 +2,12 @@ import Image from 'next/image';
 
 const GalleryItem = ({ doc }) => {
   return (
-    // <figure className="relative aspect-w-16 aspect-h-9 mb-2.5 border-2 border-secondary hover:border-primary rounded z-0">
     <figure className="relative flex flex-col justify-center align-center mb-2.5 hover:cursor-pointer z-0">
-      {/* <Image
-        src={doc.url}
-        alt={doc.title}
-        layout="fill"
-        objectFit="cover"
-        priority={true}
-      /> */}
       <img
         src={doc.url}
         alt={doc.title}
         className="non-draggable border-2 border-secondary hover:border-primary hover:shadow-lg hover:scale-105 rounded transition-all duration-300 ease-in-out"
       />
-      <figcaption className="text-center text-base-content">
-        {doc.title.slice(0, 20)}
-        {doc.title.length > 20 && '...'}
-      </figcaption>
     </figure>
   );
 };
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,10 +2,7 @@ import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'next-themes';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import initAuth from '@/firebase/initAuth';
 import '@/styles/globals.css';
-
-initAuth();
 
 function MyApp({ Component, pageProps }) {
   return (
